e517e7241e7fb4aea276d14d98436471622da488
theworstkhoi <kylekhoitv@gmail.com>
2024-08-24 22:25:10 +0700

    feat: add circle of completion for the number of solved questions

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,42 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/DashBoard.css"
-import { useEffect } from "react";
-import * as q from "../components/query"
 
 export default function DashBoard() {
   const [res, setRes] = useState("");
+  const [percentage, setPercentage] = useState(25);
 
-  useEffect(() => {
-    // const go = async () => {
-    //   const response = await fetch("https://leetcode.com/graphql/", {
-    //     method: "POST",
-    //     headers: q.headers,
-    //     body: JSON.stringify(q.userPublicProfile("tle_solver")),
-    //     mode: 'cors'
-    //   });
-    
-    //   if (response.status !== 200) {
-    //     setRes("Something when wrong, error: " + response);
-    //     return;
-    //   }
-    
-    //   const json = await response.json();
-    //   setRes(JSON.stringify(json, null, 4));
-    // }
+  // useEffect(() => {
+  //   const go = async () => {
+  //     const response = await fetch("https://graphql-proxy-server.vercel.app/user/public_profile?username=tle_solver");
+  //     const json = await response.json();
 
-    const go = async () => {
-      const response = await fetch("https://graphql-proxy-server-zjnm.vercel.app/user");
-      const json = await response.json();
+  //     setRes(JSON.stringify(json, null, 4));
+  //   }
 
-      setRes(JSON.stringify(json, null, 4));
-    }
-
-    go();
-  }, []);
+  //   go();
+  // }, []);
 
   return <div className="main-dashboard">
     <div className="num-solve box">
-      num solved
+      <svg viewBox="0 0 110 110" xmlns="http://www.w3.org/2000/svg">
+        <ellipse cx="55" cy="55" rx="50" ry="50"
+          style={{
+            fill: "none",
+            strokeWidth: 2,
+            stroke: "gray",
+          }}
+        />
+        <ellipse cx="55" cy="55" rx="50" ry="50"
+          style={{
+            fill: "none",
+            strokeWidth: 2,
+            stroke: "violet",
+            strokeDasharray: `${2 * 50 * Math.PI * percentage / 100} ${2 * 50 * Math.PI * (100 - percentage) / 100}`,
+            strokeLinecap: "round"
+          }}
+        />
+        <text x="50%" y="50%" 
+          dominant-baseline="middle"
+          text-anchor="middle"
+          className="question-percentage"
+          transform={`rotate(${percentage * 3.6 - 90}, 55, 55)`}
+        >{percentage}</text>
+      </svg>
     </div>
     <div className="difficulties box">
       3 difficulties
